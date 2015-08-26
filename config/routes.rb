1be3da0483bb3a1a99fc219819076cd91ce9e2fa
najibu <1o1jibby@gmail.com>
2015-08-26 07:29:31 +0100

    Made test page with task panel

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
 
+  get 'test' => 'pages#test'
+
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
